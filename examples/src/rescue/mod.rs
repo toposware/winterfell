959e65ef76dc1096bfa8474779a05246e7d94320
Robin Salen <salenrobin@gmail.com>
2021-12-20 16:18:31 +0900

    Update copyright mentions

--- examples/src/rescue/mod.rs
+++ examples/src/rescue/mod.rs
@@ -1,3 +1,9 @@
+// Copyright (c) Facebook, Inc. and its affiliates.
+// Copyright (c) 2021 Toposware, Inc.
+//
+// This source code is licensed under the MIT license found in the
+// LICENSE file in the root directory of this source tree.
+
 pub mod rescue_128;
 pub mod rescue_62;
 pub mod rescue_63;
