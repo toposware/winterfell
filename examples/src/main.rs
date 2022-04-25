7b853f1a33a8cf76ba7b4721710dbd6ce0334387
Robin Salen <salenrobin@gmail.com>
2022-04-25 15:13:52 +0900

    Apply review comments - Change example - Remove unnecessary code for custom trace table - Move rescue specific function into rescue_raps module

--- examples/src/main.rs
+++ examples/src/main.rs
@@ -51,6 +51,7 @@ fn main() {
         ExampleType::RescueF62 { chain_length } => rescue_62::get_example(options, chain_length),
         //ExampleType::RescueF63 { chain_length } => rescue_63::get_example(options, chain_length),
         ExampleType::RescueF128 { chain_length } => rescue_128::get_example(options, chain_length),
+        #[cfg(feature = "std")]
         ExampleType::RescueRaps { chain_length } => rescue_raps::get_example(options, chain_length),
         #[cfg(feature = "std")]
         ExampleType::Merkle { tree_depth } => merkle::get_example(options, tree_depth),
