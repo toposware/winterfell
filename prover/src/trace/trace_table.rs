47a185cee8799763347a9a1f658826d40d386e5e
Irakliy Khaburzaniya <irakliy81@gmail.com>
2022-10-23 22:53:49 -0700

    updated versions of all crates to v0.4.1

--- prover/src/trace/trace_table.rs
+++ prover/src/trace/trace_table.rs
@@ -118,7 +118,7 @@ impl<B: StarkField> TraceTable<B> {
             "execution trace length must be a power of 2"
         );
         assert!(
-            log2(length) as u32 <= B::TWO_ADICITY,
+            log2(length) <= B::TWO_ADICITY,
             "execution trace length cannot exceed 2^{} steps, but was 2^{}",
             B::TWO_ADICITY,
             log2(length)
@@ -169,7 +169,7 @@ impl<B: StarkField> TraceTable<B> {
             "execution trace length must be a power of 2"
         );
         assert!(
-            log2(trace_length) as u32 <= B::TWO_ADICITY,
+            log2(trace_length) <= B::TWO_ADICITY,
             "execution trace length cannot exceed 2^{} steps, but was 2^{}",
             B::TWO_ADICITY,
             log2(trace_length)
