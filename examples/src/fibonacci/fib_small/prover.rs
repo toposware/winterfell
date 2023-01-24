e7b31acff0c1d2467ba180aa995170dafa6e9053
Robin Salen <salenrobin@gmail.com>
2023-01-23 19:20:54 -0500

    Add license to missing files

--- examples/src/fibonacci/fib_small/prover.rs
+++ examples/src/fibonacci/fib_small/prover.rs
@@ -1,4 +1,5 @@
 // Copyright (c) Facebook, Inc. and its affiliates.
+// Copyright (c) 2021-2023 Toposware, Inc.
 //
 // This source code is licensed under the MIT license found in the
 // LICENSE file in the root directory of this source tree.
--- math/src/fft/fft_inputs.rs
+++ math/src/fft/fft_inputs.rs
@@ -1,3 +1,9 @@
+// Copyright (c) Facebook, Inc. and its affiliates.
+// Copyright (c) 2021-2023 Toposware, Inc.
+//
+// This source code is licensed under the MIT license found in the
+// LICENSE file in the root directory of this source tree.
+
 use super::{permute_index, FieldElement, StarkField};
 
 // CONSTANTS
